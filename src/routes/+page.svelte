f26b46f51d964dd2ca0f62196ebc5967a7a790fb
Mindy Ng <166593163+mindalynworld@users.noreply.github.com>
2025-09-15 22:31:19 -0700

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
-<h1>Welcome to the LAEF Impact Piece</h1>
+<h1>Welcome to Mindalyn World!</h1>
+<p>Get started on your new project</p>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
