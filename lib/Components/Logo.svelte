c8366dded3c6d004815c159d54debb0069d61e60
mindalynworld <mindalynworld@users.noreply.github.com>
2025-10-30 00:26:10 +0000

    deploy: 19bc3a76c4b9ec5d6de71c7b2699b54b193a7b38

---
+++ lib/Components/Logo.svelte
@@ -0,0 +1,104 @@
+<script lang="ts">
+    import * as d3 from 'd3';
+
+    var arts_color_faded = "rgba(195, 175, 252, 0.75)"; 
+    var edu_color_faded = "rgba(205, 232, 181, 0.75)";
+    var tech_color_faded = "rgba(252, 191, 78, 0.75)";
+
+    let arts_color = "#AB91F2";
+    let tech_color = "#F5B031";
+    let edu_color = "#81CE3C";
+
+    var colors = [arts_color, tech_color, edu_color];
+    
+    var colors_faded = [edu_color_faded, arts_color_faded, tech_color_faded];
+    let used: Set<number>;
+
+    function generateRandomIndex(maxIndex: number, used_set: Set<number>) {
+        var index = Math.round(Math.random() * maxIndex)
+        if (used_set.has(index)) {
+            if (used_set.size == maxIndex+1) {
+                return 0; // no more indices
+            }
+            index = generateRandomIndex(maxIndex, used_set);
+        }
+        used_set.add(index)
+        return index;
+    }
+    
+    import { onMount } from 'svelte';
+
+    onMount(() => {
+        init();
+    });
+
+    function init(): void {
+         // /* start logo animations */
+        recolor();
+        setInterval(recolor, 6000);
+    }
+
+    /* LOGO PULSING TRANSITIONS */
+    let recolor = function() {
+        d3.selectAll(".logo-circle")
+            .transition()
+            .delay((d: any, i: number) => i*500)
+            .duration(1000)
+            .attr("fill", (d: any, i: number) => colors[i])
+            .on("end", function() {
+                used = new Set<number>();
+
+                d3.selectAll("#technology")
+                .transition()
+                .duration(1000)
+                .attr("fill", () => colors[generateRandomIndex(2, used)])
+
+                d3.selectAll("#education")
+                .transition()
+                .delay(500)
+                .duration(1000)
+                .attr("fill", () => colors[generateRandomIndex(2, used)])
+
+                d3.selectAll("#arts")
+                .transition()
+                .delay(1000)
+                .duration(1000)
+                .attr("fill", () => colors[generateRandomIndex(2, used)])
+            });
+    }
+</script>
+
+<div class="logo">
+    <svg width="300" height="170">
+        <g>
+            <circle class="logo-circle" id="arts"
+            cx="150" cy="80" r="40"
+            fill={colors[0]}
+            />
+            <circle class="logo-circle" id="technology"
+            cx="120" cy="120" r="40"
+            fill={colors[1]}
+            />
+            <circle class="logo-circle" id="education"
+            cx="180" cy="120" r="40"
+            fill={colors[2]}
+            />
+            <text class="name" x="50" y="120">Mindy Ng</text>
+        </g>
+    </svg>
+</div>
+
+<style>
+    .logo-circle {
+        opacity: 0.75;
+        stroke: white;
+        stroke-width: 2px;
+        stroke-opacity: 0.5;
+    }
+
+    .name {
+        font-size: 3rem;
+        font-weight: 600;
+        fill: white;
+    }
+</style>
--- routes/+layout.svelte
+++ routes/+layout.svelte
@@ -3,6 +3,7 @@
     import { resolve } from '$app/paths';
 	import ButtonGroup from '$lib/Components/ButtonGroup.svelte';
 	import { goto } from "$app/navigation";
+	import Logo from '$lib/Components/Logo.svelte';
 
 	let { children } = $props();
 
@@ -17,10 +18,11 @@
 
 <div class="header"> 
 	<div class="title">
-		<img class="logo" src="{resolve(`/logo/logo-basic.png`)}" alt="three circles logo">
+		<!-- <img class="logo" src="{resolve(`/logo/logo-basic.png`)}" alt="three circles logo"> -->
+		<Logo></Logo>
 		<div>
 			<!-- <p class="name">Mindy Ng</p> -->
-			<p class="tagline">social impact through data storytelling, movement, & education</p>
+			<p class="tagline">social impact through data storytelling, movement arts, & education</p>
 			<p class="contact">
 				mindylynnng@gmail.com //&nbsp;<a href="https://www.linkedin.com/in/mindylynnng/">LinkedIn</a>
 			</p>
@@ -39,12 +41,11 @@
 	.header {
 		background-color: var(--secondary-color);
 		color: var(--primary-color);
-		height: 20rem;
 		display: flex;
 		flex-direction: column;
 		align-items: center;
 		justify-content: space-evenly;
-		padding: 0 1rem;
+		padding: 1rem;
 
 		/* @media (max-width: 480px) {
 			height: 15rem;
@@ -58,11 +59,11 @@
 		align-items: center;
 		/* height: 80%; */
 	
-		.logo {
+		/* .logo {
 			height: 8rem;
 			width: 8rem;
 			padding-right: 1rem;
-		}
+		} */
 
 		/* .name {
 			font-size: 30px;
@@ -74,6 +75,7 @@
         	display: flex;
 			margin: 1rem;
 			font-weight: 600;
+			text-align: center;
 			
 			a {
 				color: var(--primary-color);
