69416b7fed3cdf9b34f39418e4891bb8da942ce7
mindalynworld <mindalynworld@users.noreply.github.com>
2026-05-18 22:15:07 +0000

    deploy: 1007f4782c68ca3c8d85c1f58604f4283165dfea

--- routes/about/+page.svelte
+++ routes/about/+page.svelte
@@ -1,6 +1,13 @@
+<script lang="ts">
+	import Timeline from "$lib/Components/Timeline.svelte";
+</script>
+
 <div class="page">
     <h1>about</h1>
     <p>this is the about page.</p>
+
+    <h2>Experience Timeline</h2>
+    <Timeline></Timeline>
 </div>
 
 <style>
